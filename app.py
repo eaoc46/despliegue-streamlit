d239a918887d6a29b455807bea926720aa50ecbd
eaoc46 <50491590+eaoc46@users.noreply.github.com>
2025-09-18 09:32:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -51,18 +51,29 @@ data_preparada.head()
 data_preparada=data_preparada.reindex(columns=variables,fill_value=0)
 data_preparada.head()
 
-#Se normaliza la edad para predecir con Knn, Red
-#En los despliegues no se llama fit
-#data_preparada[['Edad']]= min_max_scaler.transform(data_preparada[['Edad']])
-#data_preparada.head()
-
 """# **Predicciones**"""
-
 #Hacemos la predicción con el Tree
-Y_pred = model_Tree.predict(data_preparada)
-print(Y_pred)
+Y_pred_TREE = model_Tree.predict(data_preparada)
+Y_pred_RF = model_rf.predict(data_preparada)
 
-data['Prediccion']=Y_pred
+print(Y_pred_TREE)
+print(Y_pred_RF)
+
+#Se normaliza la edad para predecir con Knn, Red
+data_preparada[['Edad']]= min_max_scaler.transform(data_preparada[['Edad']])
+data_preparada.head()
+
+Y_pred_KNN = model_Knn.predict(data_preparada)
+Y_pred_NN = model_NN.predict(data_preparada)
+
+print(Y_pred_KNN)
+print(Y_pred_NN)
+
+data['Prediccion_TREE']=Y_pred_TREE
+data['Prediccion_RF]=Y_pred_RF
+data['Prediccion_KNN']=Y_pred_KNN
+data['Prediccion_NN=Y_pred_NN
+
 data.head()
 
 #Predicciones finales
